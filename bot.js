4d1b705b728351f0666bcee98f9fd7a7230542e1
AlphaKinzaki <39883274+AlphaKinzaki@users.noreply.github.com>
2018-06-03 20:17:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDUxNDMxMjczNjMwNzkzNzI4.DfWzfw.lhGd4_6FAFAM2meODf1adju1T9Y);
+client.login(process.env.BOT_TOKEN);
